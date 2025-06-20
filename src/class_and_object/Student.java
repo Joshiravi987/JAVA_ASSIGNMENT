14f540b3b8a393fad42177190d6da74ee0ca4317
Ravi <joshiravi987@gmail.com>
2025-06-20 12:00:50 +0530

    added JAVA Assignment files

---
+++ src/class_and_object/Student.java
@@ -0,0 +1,13 @@
+package class_and_object;
+
+public class Student {
+	//Create a class Student with attributes (name, age) and a method to display the  details.
+public String name;
+public int age;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
