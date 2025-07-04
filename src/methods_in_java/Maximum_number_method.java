24a81cff9ecfaff973b382463e98e159d8a58e02
Ravi <joshiravi987@gmail.com>
2025-07-04 12:58:57 +0530

    added JAVA  files

--- src/methods_in_java/Maximum_number_method.java
+++ src/methods_in_java/Maximum_number_method.java
@@ -17,10 +17,23 @@ int max_number() {
 		return c;
 	}
 }
+	public int min_number() {
+	int min;
+	if (a<= b && a<=c) {
+		return a;
+	}
+	else if (b<=a && b<=c) {
+		return b;
+	}
+	else {
+		return c;
+	}
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		Maximum_number_method obj = new Maximum_number_method();
+		
 		System.out.println("Enter first number a =");
 		obj.a = sc.nextInt();
 		System.out.println("Enter second number b =");
@@ -32,6 +45,8 @@ int max_number() {
 		System.out.println("THe max number is :" + max);
 		sc.close();
 
+		int min = obj.min_number();
+		System.out.println("The min number is :" +min);
 	}
 
 }
